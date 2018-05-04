05ac4124ab115bda6ad3d8807f97b433cf7a5e64
taufiqfuad <taufikfuadditlantas@gmail.com>
2018-05-04 14:00:20 +0800

    mengisi nilai attribut

--- src/praktek02/TriangleAksi.java
+++ src/praktek02/TriangleAksi.java
@@ -2,5 +2,7 @@ package praktek02;
 public class TriangleAksi {
     public static void main(String[] args) {
         Triangle t1 = new Triangle();
+        t1.alas = 20;
+        t1.tinggi = 10;
     }
 }
